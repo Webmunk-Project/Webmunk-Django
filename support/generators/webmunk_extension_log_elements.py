d084637865c752a6724f57d99a51517b7d5f0b7c
Chris J. Karr <chris@audacious-software.com>
2022-07-03 20:18:22 +0000

    Exporter fixes

--- support/generators/webmunk_extension_log_elements.py
+++ support/generators/webmunk_extension_log_elements.py
@@ -7,9 +7,15 @@ import io
 import os
 import tempfile
 
+from zipfile import ZipFile
+
+import arrow
 import pytz
 
+from past.utils import old_div
+
 from django.conf import settings
+from django.utils.text import slugify
 
 from passive_data_kit.models import DataPoint, DataSource, DataGeneratorDefinition, DataSourceReference
 
@@ -20,31 +26,11 @@ def generator_name(identifier): # pylint: disable=unused-argument
     return 'Webmunk: Static Page Elements Log'
 
 def compile_report(generator, sources, data_start=None, data_end=None, date_type='created'): # pylint: disable=too-many-locals, too-many-branches, too-many-statements
-    filename = tempfile.gettempdir() + os.path.sep + generator + '.txt'
+    now = arrow.get()
 
-    log_elements_reference = DataGeneratorDefinition.definition_for_identifier('webmunk-extension-log-elements')
+    zip_filename = tempfile.gettempdir() + os.path.sep + 'pdk_export_' + str(now.timestamp) + str(old_div(now.microsecond, 1e6)) + '.zip'
 
-    with io.open(filename, 'w', encoding='utf-8') as outfile:
-        writer = csv.writer(outfile, delimiter='\t')
-
-        columns = [
-            'Source',
-            'Date Created',
-            'Date Recorded',
-            'Time Zone',
-            'Tab ID',
-            'URL',
-            'Page Title',
-            'Pattern',
-            'Top',
-            'Left',
-            'Width',
-            'Height',
-            'Element HTML',
-        ]
-
-        writer.writerow(columns)
-
+    with ZipFile(zip_filename, 'w', allowZip64=True) as export_file:
         for source in sorted(sources): # pylint: disable=too-many-nested-blocks
             data_source = DataSource.objects.filter(identifier=source).first()
 
@@ -53,77 +39,113 @@ def compile_report(generator, sources, data_start=None, data_end=None, date_type
 
                 source_ref = DataSourceReference.reference_for_source(source)
 
-                if data_end is not None and data_start is not None:
-                    if (data_end - data_start).days < 1:
-                        data_start = data_end - datetime.timedelta(days=1)
+                secondary_filename = tempfile.gettempdir() + os.path.sep + generator + '__' + source + '.txt'
 
-                date_sort = '-created'
+                log_elements_reference = DataGeneratorDefinition.definition_for_identifier('webmunk-extension-log-elements')
 
-                points = DataPoint.objects.filter(source_reference=source_ref, generator_definition=log_elements_reference)
+                with io.open(secondary_filename, 'w', encoding='utf-8') as outfile:
+                    writer = csv.writer(outfile, delimiter='\t')
 
-                if date_type == 'created':
-                    if data_start is not None:
-                        points = points.filter(created__gte=data_start)
+                    columns = [
+                        'Source',
+                        'Date Created',
+                        'Date Recorded',
+                        'Time Zone',
+                        'Tab ID',
+                        'URL',
+                        'Page Title',
+                        'Pattern',
+                        'Top',
+                        'Left',
+                        'Width',
+                        'Height',
+                        'Element HTML',
+                    ]
 
-                    if data_end is not None:
-                        points = points.filter(created__lte=data_end)
+                    writer.writerow(columns)
 
-                if date_type == 'recorded':
-                    if data_start is not None:
-                        points = points.filter(recorded__gte=data_start)
+                    if data_end is not None and data_start is not None:
+                        if (data_end - data_start).days < 1:
+                            data_start = data_end - datetime.timedelta(days=1)
 
-                    if data_end is not None:
-                        points = points.filter(recorded__lte=data_end)
+                    date_sort = '-created'
 
-                    date_sort = '-recorded'
+                    points = DataPoint.objects.filter(source_reference=source_ref, generator_definition=log_elements_reference)
 
-                points = points.order_by(date_sort)
+                    if date_type == 'created':
+                        if data_start is not None:
+                            points = points.filter(created__gte=data_start)
 
-                for point in points:
-                    properties = point.fetch_properties()
+                        if data_end is not None:
+                            points = points.filter(created__lte=data_end)
 
-                    for pattern in properties.get('pattern-matches', {}).keys():
-                        for pattern_match in properties.get('pattern-matches', {}).get(pattern, []):
-                            try:
-                                row = []
+                    if date_type == 'recorded':
+                        if data_start is not None:
+                            points = points.filter(recorded__gte=data_start)
 
-                                row.append(point.source)
+                        if data_end is not None:
+                            points = points.filter(recorded__lte=data_end)
 
-                                tz_str = properties['passive-data-metadata'].get('timezone', settings.TIME_ZONE)
+                        date_sort = '-recorded'
 
-                                here_tz = pytz.timezone(tz_str)
+                    points = points.order_by(date_sort)
 
-                                created = point.created.astimezone(here_tz)
-                                recorded = point.recorded.astimezone(here_tz)
+                    point_count = points.count()
 
-                                row.append(created.isoformat())
-                                row.append(recorded.isoformat())
+                    points_index = 0
 
-                                row.append(tz_str)
+                    while points_index < point_count:
+                        for point in points[points_index:(points_index + 1000)]:
+                            properties = point.fetch_properties()
 
-                                row.append(properties.get('tab-id', ''))
+                            for pattern in properties.get('pattern-matches', {}).keys():
+                                for pattern_match in properties.get('pattern-matches', {}).get(pattern, []):
+                                    try:
+                                        row = []
 
-                                here_tz = pytz.timezone(tz_str)
+                                        row.append(point.source)
 
-                                row.append(properties.get('url!', ''))
-                                row.append(properties.get('page-title!', ''))
+                                        tz_str = properties['passive-data-metadata'].get('timezone', settings.TIME_ZONE)
 
-                                row.append(pattern)
+                                        here_tz = pytz.timezone(tz_str)
 
-                                size = pattern_match.get('size', {})
-                                offset = pattern_match.get('offset', {})
+                                        created = point.created.astimezone(here_tz)
+                                        recorded = point.recorded.astimezone(here_tz)
 
-                                row.append(offset.get('top', ''))
-                                row.append(offset.get('left', ''))
-                                row.append(size.get('width', ''))
-                                row.append(size.get('height', ''))
+                                        row.append(created.isoformat())
+                                        row.append(recorded.isoformat())
 
-                                row.append(remove_newlines(pattern_match.get('element-content!', '')))
+                                        row.append(tz_str)
 
-                                writer.writerow(row)
-                            except TypeError:
-                                pass
-                            except AttributeError:
-                                pass
+                                        row.append(properties.get('tab-id', ''))
 
-    return filename
+                                        here_tz = pytz.timezone(tz_str)
+
+                                        row.append(properties.get('url!', ''))
+                                        row.append(properties.get('page-title!', ''))
+
+                                        row.append(pattern)
+
+                                        size = pattern_match.get('size', {})
+                                        offset = pattern_match.get('offset', {})
+
+                                        row.append(offset.get('top', ''))
+                                        row.append(offset.get('left', ''))
+                                        row.append(size.get('width', ''))
+                                        row.append(size.get('height', ''))
+
+                                        row.append(remove_newlines(pattern_match.get('element-content!', '')))
+
+                                        writer.writerow(row)
+                                    except TypeError:
+                                        pass
+                                    except AttributeError:
+                                        pass
+
+                        points_index += 1000
+
+                export_file.write(secondary_filename, slugify(generator) + '/' + slugify(source) + '.txt')
+
+                os.remove(secondary_filename)
+
+    return zip_filename
