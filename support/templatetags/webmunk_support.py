8d0c848ea122c15ec5d95c8061df65bd3ec0d7aa
Chris J. Karr <chris@audacious-software.com>
2022-10-12 17:15:18 +0000

    Misc. bug fixes + support for remote group reassignment

--- support/templatetags/webmunk_support.py
+++ support/templatetags/webmunk_support.py
@@ -5,6 +5,7 @@ import datetime
 from django import template
 
 from django.conf import settings
+from django.utils import timezone
 
 register = template.Library()
 
@@ -12,7 +13,20 @@ register = template.Library()
 def needs_final_upload(data_request):
     followup_date = data_request.requested + datetime.timedelta(days=settings.WEBMUNK_DATA_FOLLOWUP_DAYS)
 
+    if timezone.now() < followup_date:
+        return False
+
     if data_request.data_files.filter(uploaded__gte=followup_date).count() == 0:
+        return True
+
+    return False
+
+@register.filter(name='still_needs_uploads')
+def still_needs_uploads(data_request):
+    if data_request.data_files.all().count() == 0:
+        return True
+
+    if needs_final_upload(data_request):
         return True
 
     return False
