c5b736f57a87d2e2da3a58cf8bfdc4b83d237561
Chris J. Karr <chris@audacious-software.com>
2023-06-07 12:54:14 +0000

    Updated nightly filenames

--- support/management/commands/webmunk_create_nightly_export_job.py
+++ support/management/commands/webmunk_create_nightly_export_job.py
@@ -69,14 +69,7 @@ class Command(BaseCommand):
         parameters['date_type'] = 'recorded'
         parameters['path'] = yesterday.strftime('%Y-%m-%d/')
 
-        source_range = ''
-
-        if len(active_users) == 1:
-            source_range = active_users[0]
-        elif len(active_users) >= 2:
-            source_range = '%s.%s' % (active_users[0], active_users[-1])
-
-        prefix = ('%s_%s_%s' % (settings.ALLOWED_HOSTS[0], yesterday.isoformat(), source_range))
+        prefix = ('%s_%s' % (settings.ALLOWED_HOSTS[0], yesterday.isoformat(),))
 
         parameters['prefix'] = prefix
 
