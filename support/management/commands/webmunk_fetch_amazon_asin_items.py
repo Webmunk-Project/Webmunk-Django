213c3039c551b8b4b41ec2c583e7033fffd60c67
Chris J. Karr <chris@audacious-software.com>
2023-10-19 12:59:11 +0000

    ASIN processing work

--- support/management/commands/webmunk_fetch_amazon_asin_items.py
+++ support/management/commands/webmunk_fetch_amazon_asin_items.py
@@ -9,7 +9,7 @@ from django.db.models import Q
 from django.utils import timezone
 
 from passive_data_kit.decorators import handle_lock
-from passive_data_kit.models import DataPoint
+from passive_data_kit.models import DataPoint, DataGeneratorDefinition
 
 from ...models import AmazonASINItem
 
@@ -21,30 +21,56 @@ class Command(BaseCommand):
 
     @handle_lock
     def handle(self, *args, **options): # pylint: disable=too-many-branches, too-many-locals
-        query = None
-
         latest_asin_item = AmazonASINItem.objects.all().order_by('-pk').first()
 
-        seconds_window = 0
+        orders = DataGeneratorDefinition.definition_for_identifier('webmunk-amazon-order')
+        elements = DataGeneratorDefinition.definition_for_identifier('webmunk-extension-log-elements')
+
+        point_query = Q(generator_definition=orders)
+        point_query = point_query | Q(generator_definition=elements)
+        # point_query = point_query | Q(secondary_identifier='webmunk-asin-item')
+
+        query = None
+
+        point_count = 0
 
         if latest_asin_item is not None:
-            while query is None or DataPoint.objects.filter(query).count() == 0:
-                seconds_window = seconds_window + 60
+            window_start = latest_asin_item.added
 
-                window_end = latest_asin_item.added + datetime.timedelta(seconds=seconds_window)
+            while query is None or point_count == 0:
+                window_end = window_start + datetime.timedelta(seconds=1800)
 
-                query = Q(secondary_identifier='webmunk-asin-item') | Q(generator_identifier='webmunk-amazon-order')
-                query = query & Q(created__gt=latest_asin_item.added)
+                query = point_query & Q(created__gt=window_start)
                 query = query & Q(created__lte=window_end)
 
                 if window_end > timezone.now():
                     break
+
+                point_count = DataPoint.objects.filter(query).count()
+
+                # print('FOUND BETWEEN %s -- %s --> %s -- %s' % (window_start, window_end, point_count, timezone.now()))
+
+                window_start = window_end
         else:
-            query = Q(secondary_identifier='webmunk-asin-item') | Q(generator_identifier='webmunk-amazon-order')
+            query = point_query
 
         last_created = None
 
-        for point in DataPoint.objects.filter(query).order_by('created')[:250]:
+        index = 0
+
+        points = DataPoint.objects.filter(query).order_by('created')
+
+        point_pks = points.values_list('pk', flat=True)
+
+        saved = 0
+
+        for point_pk in point_pks:
+            point = DataPoint.objects.get(pk=point_pk)
+
+            # print('INDEX: %s / %s -- %s -- %s' % (index, point_count, point.generator_identifier, timezone.now()))
+
+            index += 1
+
             asins = []
 
             props = point.fetch_properties()
@@ -57,9 +83,7 @@ class Command(BaseCommand):
                         # print('ADDED FROM ORDER: %s' % item_asin)
                         asins.append(item_asin)
 
-            else:
-                props = point.fetch_properties()
-
+            elif point.generator_identifier == 'webmunk-extension-log-elements':
                 page_url = props.get('url!', props.get('url*', None))
 
                 if page_url is not None and '/dp/' in page_url:
@@ -70,10 +94,43 @@ class Command(BaseCommand):
                             # print('ADDED FROM URL: %s' % matched_item)
                             asins.append(matched_item)
 
+                for key, ele_matches in props.get('pattern-matches', {}).items():
+                    for ele_match in ele_matches:
+                        element = ele_match.get('element-content!', ele_match.get('element-content*', None))
+
+                        # print('ELEMENT LEN: %s' % len(element))
+
+                        if element is not None:
+                            matches = re.findall('/dp/(.+?)/', element)
+
+                            for matched_item in matches:
+                                if ('?' in matched_item) is False and (matched_item in asins) is False:
+                                    # print('ADDED FROM ELEMENT URL: %s' % matched_item)
+                                    asins.append(matched_item)
+
+                            matches = re.findall('data-asin="(.+?)"', element)
+
+                            for matched_item in matches:
+                                if (matched_item in asins) is False:
+                                    # print('ADDED FROM ELEMENT DATA-ASIN: %s' % matched_item)
+                                    asins.append(matched_item)
+            else:
+                props = point.fetch_properties()
+
+                page_url = props.get('url!', props.get('url*', None))
+
+                if page_url is not None and '/dp/' in page_url:
+                    matches = re.findall('/dp/(.+?)/', page_url)
+
+                    for matched_item in matches:
+                        if ('?' in matched_item) is False and (matched_item in asins) is False:
+                            # print('ADDED FROM URL: %s' % matched_item)
+                            asins.append(matched_item)
+
                 element = props.get('element-content!', props.get('element-content*', None))
 
                 if element is not None:
-                    matches = re.findall('.*/dp/(.+?)/.*', element)
+                    matches = re.findall('/dp/(.+?)/', element)
 
                     for matched_item in matches:
                         if ('?' in matched_item) is False and (matched_item in asins) is False:
@@ -93,8 +150,29 @@ class Command(BaseCommand):
 
                     asin = tokens[0]
 
+                if ')' in asin:
+                    tokens = asin.split(')')
+
+                    asin = tokens[0]
+
+                if '&' in asin:
+                    tokens = asin.split('&')
+
+                    asin = tokens[0]
+
+                if '<' in asin:
+                    tokens = asin.split('<')
+
+                    asin = tokens[0]
+
+                # print('CHECKING ASIN: %s' % asin)
+
                 if AmazonASINItem.objects.filter(asin=asin).count() == 0:
                     AmazonASINItem.objects.create(asin=asin, added=point.created, updated=point.created)
+
+                    # print('SAVED ASIN: %s' % asin)
+
+                    saved += 1
 
             if last_created is None or point.created > last_created:
                 last_created = point.created
@@ -105,3 +183,6 @@ class Command(BaseCommand):
             AmazonASINItem.objects.filter(asin__startswith='20', asin__contains='T').filter(asin__contains=':').filter(asin__contains='+').delete()
 
             AmazonASINItem.objects.create(asin=last_created.isoformat(), added=last_created, updated=last_created)
+
+        # if saved > 0:
+        #    print('Saved: %s' % saved)
--- support/views.py
+++ support/views.py
@@ -2,7 +2,7 @@
 
 import simplejson as json
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import get_object_or_404
 
 from .models import AmazonASINItem
@@ -10,6 +10,11 @@ from .models import AmazonASINItem
 def asin_json(request, asin): # pylint: disable=unused-argument
     asin_item = get_object_or_404(AmazonASINItem, asin__iexact=asin)
 
-    metadata = json.loads(asin_item.metadata)
+    try:
+        metadata = json.loads(asin_item.metadata)
 
-    return HttpResponse(json.dumps(metadata, indent=2, ignore_nan=True, default=None), content_type='application/json')
+        return HttpResponse(json.dumps(metadata, indent=2, ignore_nan=True, default=None), content_type='application/json')
+    except TypeError:
+        pass
+
+    return HttpResponseNotFound('Item not yet populated')
